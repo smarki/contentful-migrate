97a949c5b8099cf0ad5e3774ff878061244ed156
S. Markidis <stmarkidis@gmail.com>
2023-03-29 09:29:13 +0200

    chore: fix template formatting

---
+++ lib/template.ts
@@ -0,0 +1,20 @@
+// @ts-check
+import { MigrationFunction } from "contentful-migration";
+
+export const description = "<Put your description here>";
+
+/**
+ * @param {import("contentful-migration").default} migration
+ */
+export const up: MigrationFunction = (migration) => {
+  // Add your UP migration script here. See examples here:
+  // https://github.com/contentful/migration-cli/tree/master/examples
+};
+
+/**
+ * @param {import("contentful-migration").default} migration
+ */
+export const down: MigrationFunction = (migration) => {
+  // Add your DOWN migration script here. See examples here:
+  // https://github.com/contentful/migration-cli/tree/master/examples
+};
